761917d28b7252d0686e2ef21c19cd21110d4b6b
David02345 <02william02@hanmail.net>
2026-07-10 03:05:27 +0900

    motor added

--- src/ap/ap.c
+++ src/ap/ap.c
@@ -9,7 +9,7 @@
 
 void apInit(void)
 {
-  uartOpen(_DEF_UART2, 57600);
+  uartOpen(_DEF_UART1, 57600);
 }
 
 void apMain(void)
@@ -20,7 +20,7 @@ void apMain(void)
   uint32_t prev_adc_count = 0;
 
   delay(1000);
-  motorStart();
+  //motorStart();
 
   delay(500);
   //motorOpenLoopStart();
@@ -34,7 +34,7 @@ void apMain(void)
     if (now - pre_time_motor_slow >= 10)
     {
       pre_time_motor_slow = now;
-      motorLowSpeedTask();
+      //motorLowSpeedTask();
     }
 
     if (now - pre_time_debug >= 500)
@@ -48,11 +48,11 @@ void apMain(void)
       uint32_t adc_hz = adc_diff * 2U;   // 500ms 기준이므로 x2
       int32_t vbus_mv = (int32_t)(motorGetVbus() * 1000.0f);
 
-      uartPrintf(_DEF_UART2, "ADC Count : %lu\r\n", (unsigned long)adc_count);
-      uartPrintf(_DEF_UART2, "ADC Hz    : %lu\r\n", (unsigned long)adc_hz);
-      uartPrintf(_DEF_UART2, "State     : %d\r\n", (int)motorGetState());
-      uartPrintf(_DEF_UART2, "Fault     : %d\r\n", (int)motorGetFault());
-      uartPrintf(_DEF_UART2, "VBUS      : %ld mV\r\n", (long)vbus_mv);
+      uartPrintf(_DEF_UART1, "ADC Count : %lu\r\n", (unsigned long)adc_count);
+      uartPrintf(_DEF_UART1, "ADC Hz    : %lu\r\n", (unsigned long)adc_hz);
+      uartPrintf(_DEF_UART1, "State     : %d\r\n", (int)motorGetState());
+      uartPrintf(_DEF_UART1, "Fault     : %d\r\n", (int)motorGetFault());
+      uartPrintf(_DEF_UART1, "VBUS      : %ld mV\r\n", (long)vbus_mv);
     }
   }
 }
